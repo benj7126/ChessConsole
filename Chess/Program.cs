aa3b4a1881e25cc0ba35684d1f6ec233f69483b6
catNull <benvb2004@gmail.com>
2022-01-03 09:18:06 +0100

    Add project files.

---
+++ Chess/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int[,] board = new int[7, 7];
+        board[5, 1] = 2;
+        Console.WriteLine(board[5, 1]);
+    }
+}
